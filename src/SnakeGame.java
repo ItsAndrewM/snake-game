ec84c97b85d89952b90e8c90d66ab06387c6aefa
Andrew McMillan <andrewmcmillan@precisionsailloft.com>
2023-07-07 15:55:47 -0700

    snake game init

---
+++ src/SnakeGame.java
@@ -0,0 +1,12 @@
+
+//https://www.youtube.com/watch?v=bI6e6qjJ8JQ
+
+public class SnakeGame {
+    public static void main(String[] args) {
+
+        // GameFrame frame = new GameFrame();
+        new GameFrame();
+
+
+    }
+}
