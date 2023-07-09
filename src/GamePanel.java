e2995ba0f6678810c7fdd57eaed755c94bf1bd05
Andrew McMillan <mcmillan.r.andrew@gmail.com>
2023-07-08 20:17:50 -0700

    added documentation

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -13,11 +13,15 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int DELAY = 75;
     final int x[] = new int[GAME_UNITS];
     final int y[] = new int[GAME_UNITS];
+    //initial body parts can be whatever
     int bodyParts = 6;
+    //applesEaten tracks when an apple is eaten and adds to score
     int applesEaten = 0;
+    //grid coordinates for apple
     int appleX;
     int appleY;
     char direction = 'R'; //R, L, U, D
+    //when game starts, running is true
     boolean running = false;
     Timer timer;
     Random random;
@@ -46,47 +50,59 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void draw(Graphics g) {
         if(running) {
+
+            //adds grid lines if wanted
             /*for (int i = 0; i < SCREEN_HEIGHT/UNIT_SIZE; i++) {
                 g.drawLine(i*UNIT_SIZE, 0, i*UNIT_SIZE, SCREEN_HEIGHT);
                 g.drawLine(0, i*UNIT_SIZE, SCREEN_WIDTH, i*UNIT_SIZE);
             }*/
+
+            //draw apple
             g.setColor(Color.red);
             g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
+            //draws snake head
             for (int i = 0; i < bodyParts; i++) {
                 if (i == 0) {
                     g.setColor(Color.green);
                     g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 }
                 else {
+                    //draws snake body
                     g.setColor(new Color(45, 180, 0));
+                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+
                     //sets a random color on the snake body
                     /* g.setColor(new Color(random.nextInt(255), random.nextInt(255), random.nextInt(255))); */
-                    g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
                 }
             }
-            //score text
+
+            //draws score text
             g.setColor(Color.red);
             g.setFont(new Font("Ink Free", Font.BOLD, 40));
             FontMetrics metrics = getFontMetrics(g.getFont());
             g.drawString("Score: " + applesEaten, (SCREEN_WIDTH - metrics.stringWidth("Score: " + applesEaten))/2, g.getFont().getSize());
         }
         else {
+            //if !running then gameover
             gameOver(g);
         }
     }
 
     public void newApple() {
+        //new random coordinates for the next/first apple
         appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
         appleY = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
 
     }
     
     public void move() {
+        //every time head moves, coordinates are moved to the following body part
         for (int i = bodyParts; i > 0; i--) {
             x[i] = x[i-1];
             y[i] = y[i-1];
         }
+        //when direction is changed, direction variable changes to match
         switch(direction) {
             case 'U': 
                 y[0] = y[0] - UNIT_SIZE;
--- src/SnakeGame.java
+++ src/SnakeGame.java
@@ -1,12 +1,9 @@
-
+//Snake Game based off a guide from the below youtube video
 //https://www.youtube.com/watch?v=bI6e6qjJ8JQ
 
 public class SnakeGame {
     public static void main(String[] args) {
-
-        // GameFrame frame = new GameFrame();
+        //call new game fram, initalizes panel, snake and apple methods
         new GameFrame();
-
-
     }
 }
